af07dddaa10e6226ae0dceda5021d18c6153dc56
Salomé <salome.acosta@correounivalle.edu.co>
2026-05-27 19:37:14 -0500

    Product Model

---
+++ api/models/product.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "El nombre es requerido"],
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: [true, "La descripción es requerida"],
+  },
+
+  color: {
+    type: String,
+    required: true,
+  },
+
+  dimensions: {
+    type: String,
+    required: true,
+  },
+
+  materials: {
+    type: [String],
+    required: true,
+  },
+
+  type: {
+    type: String,
+    required: true,
+  },
+
+  photo: {
+    type: String,
+    required: true,
+  },
+
+  code: {
+    type: String,
+    unique: true,
+  },
+});
+
+ProductSchema.pre("save", function () {
+  if (!this.code) {
+    this.code = `PRD-${Math.floor(100000 + Math.random() * 900000)}`;
+  }
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const userRoutes = require("./userRoutes");
 
 const router = express.Router();
+const productRoutes = require("./productRoutes");
 
 
 router.use("/users", userRoutes);
-
+router.use("/products", productRoutes);
 
 
 router.get("/", (req, res) => {
